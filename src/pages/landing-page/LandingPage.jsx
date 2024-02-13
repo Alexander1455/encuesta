4df01d1dc600088645008d1b7d4c31b7605d8156
Alexander1455 <alexander21111121@gmail.cm>
2024-02-13 11:26:03 -0500

    landig

--- src/pages/landing-page/LandingPage.jsx
+++ src/pages/landing-page/LandingPage.jsx
@@ -3,7 +3,7 @@ const LandigPage = () => {
     <div className="">
       <div>
         {/*introducction*/}
-        <section className="grid grid-cols-1 2xl:grid-cols-2 bg-cyan-600 items-center border-2 border-x-indigo-500">
+        <section className="grid grid-cols-1 2xl:grid-cols-2 bg-[#3a79c0] items-center">
 
             <div className="px-40 py-24 lg:w-3/4">
               <div className="w-full">
@@ -32,13 +32,13 @@ const LandigPage = () => {
         </section>
 
         {/*Sobre Nosotros*/}
-        <section className="bg-sky-600 size">
+        <section className="bg-[#4e86c6] size">
           <div className="mx-auto max-w-5xl px-6 py-16 text-center">
             <div className="flex flex-col items-center justify-center">
               <h2 className="text-3xl font-semibold text-gray-800">
                 Sobre Nosotros
               </h2>
-              <div className="w-72 border-b-4 border-orange-300"></div>
+              <div className="w-72 border-b-4 py-1 border-orange-300"></div>
             </div>
             <p className="mt-20 text-white text-2xl">
               Bienvenido a EncuestaPlus, la plataforma líder en la creación y
@@ -50,10 +50,10 @@ const LandigPage = () => {
         </section>
 
         {/*Team*/}
-        <section className="container mx-auto mt-20 content-normal py-10 ">
+        <section className="py-10 bg-[#6194cd]">
           <div className="mb-12 flex flex-col items-center justify-center">
             <h2 className="text-5xl font-semibold text-gray-800">Team</h2>
-            <div className="w-24 border-b-4 border-yellow-400"></div>
+            <div className="w-24 border-b-4 py-1 border-yellow-400"></div>
           </div>
           <div className="grid gap-2 md:grid-cols-4 content-center">
             <div className="relative mx-1">
@@ -66,7 +66,7 @@ const LandigPage = () => {
             <div className="relative mx-1">
               <div className="rounded-lg bg-white shadow-md">
                 <img
-                  src="./src/images/Hola.jpg"
+                  src="./src/images/Alex.webp"
                   className="w-full rounded-t-lg"
                 />
                 <div className="p-6">
@@ -74,10 +74,7 @@ const LandigPage = () => {
                     Alexander Rivera
                   </h2>
                   <a href="#" className="text-base">
-                    Amante de la tecnología y la toma de decisiones basada en
-                    datos, Juan es el cerebro detrás de EncuestaPlus. Con una
-                    visión clara de empoderar a las personas a través de sus
-                    opiniones, lidera el camino hacia un mundo más informado.
+                  Encargado del desarrollo del frontend. Alexander se encarga de diseñar y desarrollar la interfaz de usuario de EncuestaPlus. Su enfoque está en crear una experiencia de usuario intuitiva y atractiva que permita a los usuarios interactuar fácilmente con la plataforma.
                   </a>
                 </div>
               </div>
@@ -85,18 +82,15 @@ const LandigPage = () => {
             <div className="relative mx-1">
               <div className="rounded-lg bg-white shadow-md">
                 <img
-                  src="https://source.unsplash.com/collection/190727/300x300"
+                  src="./src/images/Diego.webp"
                   className="w-full rounded-t-lg"
                 />
-                <div className="p-6">
+                <div className="p-5">
                   <h2 className="mb-2 text-2xl font-medium text-gray-800">
                     Diego Monroy
                   </h2>
                   <a href="#" className="text-base">
-                    Con habilidades técnicas excepcionales y una pasión por la
-                    innovación, María es la fuerza impulsora detrás de la
-                    plataforma EncuestaPlus. Su dedicación a la perfección
-                    técnica garantiza una experiencia de usuario sin igual.
+                  Encargado del desarrollo del backend. Diego se encarga de construir y mantener la infraestructura tecnológica que respalda el funcionamiento de EncuestaPlus. Esto incluye el manejo de bases de datos, la implementación de lógica de negocio y la garantía de un rendimiento óptimo y seguro del sistema.
                   </a>
                 </div>
               </div>
@@ -105,29 +99,31 @@ const LandigPage = () => {
         </section>
 
         {/*Servicios*/}
-        <section className="bg-slate-700">
+        <section className="bg-[#346dad]">
           <div className="container mx-auto px-24 py-24">
             <div className="mb-12 flex flex-col items-center justify-center">
               <h3 className="text-3xl">Our Services</h3>
-              <div className="w-24 border-b-4 border-yellow-400"></div>
+              <div className="w-48 border-b-4 py-1 border-yellow-400"></div>
             </div>
             <div className="m-4 flex flex-wrap text-center">
               <div className="w-full p-4 lg:w-1/3">
                 <div className="rounded-lg border-2 border-gray-200 px-4 py-6">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="mb-3 inline-block h-20 w-20 text-yellow-300"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
+
+                  <svg 
+                  xmlns="http://www.w3.org/2000/svg" 
+                  fill="none" 
+                  viewBox="0 0 24 24" 
+                  stroke-width="1.5" 
+                  stroke="currentColor" 
+                  className="mb-3 inline-block h-20 w-20 text-yellow-300"
                   >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"
-                    />
+                  <path 
+                  stroke-linecap="round" 
+                  stroke-linejoin="round"
+                  stroke-width="2" 
+                  d="M12 10.5v6m3-3H9m4.06-7.19-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z" />
                   </svg>
+
                   <h2 className="title-font text-3xl font-medium text-gray-900">
                     Creación de Encuestas Personalizadas
                   </h2>
@@ -138,20 +134,20 @@ const LandigPage = () => {
               </div>
               <div className="w-full p-4 lg:w-1/3">
                 <div className="rounded-lg border-2 border-gray-200 px-4 py-6">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="mb-3 inline-block h-20 w-20 text-yellow-300"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
+                  <svg 
+                  xmlns="http://www.w3.org/2000/svg" 
+                  fill="none" 
+                  viewBox="0 0 24 24"
+                  stroke="currentColor" 
+                  className="mb-3 inline-block h-20 w-20 text-yellow-300"
                   >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"
-                    />
+                  <path 
+                  stroke-linecap="round" 
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M7.5 14.25v2.25m3-4.5v4.5m3-6.75v6.75m3-9v9M6 20.25h12A2.25 2.25 0 0 0 20.25 18V6A2.25 2.25 0 0 0 18 3.75H6A2.25 2.25 0 0 0 3.75 6v12A2.25 2.25 0 0 0 6 20.25Z" />
                   </svg>
+
                   <h2 className="title-font text-3xl font-medium text-gray-900">
                     Estadísticas Detalladas
                   </h2>
@@ -162,20 +158,22 @@ const LandigPage = () => {
               </div>
               <div className="w-full p-4 lg:w-1/3">
                 <div className="rounded-lg border-2 border-gray-200 px-4 py-6">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
+                  <svg 
+                    xmlns="http://www.w3.org/2000/svg" 
+                    fill="none" 
+                    viewBox="0 0 24 24" 
+                    stroke-width="1.5" 
+                    stroke="currentColor" 
                     className="mb-3 inline-block h-20 w-20 text-yellow-300"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
                   >
-                    <path
-                      stroke-linecap="round"
+                    <path 
+                      stroke-linecap="round" 
                       stroke-linejoin="round"
                       stroke-width="2"
-                      d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"
+                      d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
                     />
                   </svg>
+
                   <h2 className="title-font text-3xl font-medium text-gray-900">
                     Búsqueda de Encuestas
                   </h2>
@@ -187,140 +185,7 @@ const LandigPage = () => {
             </div>
           </div>
         </section>
-
-        {/*Formulario*/}
-        <div className="rounded-lg border-2 border-gray-200 px-4 py-6 ">
-          <div className="title-font text-3xl font-medium text-gray-900 max-w-md mx-auto py-8">
-            Registrate
-          </div>
-          <form className="max-w-md mx-auto mb-12  flex-col">
-            <div className="relative z-0 w-full mb-5 group">
-              <input
-                type="email"
-                name="floating_email"
-                id="floating_email"
-                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                placeholder=" "
-                required
-              />
-              <label
-                for="floating_email"
-                className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-              >
-                Email address
-              </label>
-            </div>
-            <div className="relative z-0 w-full mb-5 group">
-              <input
-                type="password"
-                name="floating_password"
-                id="floating_password"
-                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                placeholder=" "
-                required
-              />
-              <label
-                for="floating_password"
-                className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-              >
-                Password
-              </label>
-            </div>
-            <div className="relative z-0 w-full mb-5 group">
-              <input
-                type="password"
-                name="repeat_password"
-                id="floating_repeat_password"
-                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                placeholder=" "
-                required
-              />
-              <label
-                for="floating_repeat_password"
-                className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-              >
-                Confirm password
-              </label>
-            </div>
-            <div className="grid md:grid-cols-2 md:gap-6">
-              <div className="relative z-0 w-full mb-5 group">
-                <input
-                  type="text"
-                  name="floating_first_name"
-                  id="floating_first_name"
-                  className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                  placeholder=" "
-                  required
-                />
-                <label
-                  for="floating_first_name"
-                  className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                >
-                  First name
-                </label>
-              </div>
-              <div className="relative z-0 w-full mb-5 group">
-                <input
-                  type="text"
-                  name="floating_last_name"
-                  id="floating_last_name"
-                  className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                  placeholder=" "
-                  required
-                />
-                <label
-                  for="floating_last_name"
-                  className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                >
-                  Last name
-                </label>
-              </div>
-            </div>
-            <div className="grid md:grid-cols-2 md:gap-6">
-              <div className="relative z-0 w-full mb-5 group">
-                <input
-                  type="tel"
-                  pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-                  name="floating_phone"
-                  id="floating_phone"
-                  className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                  placeholder=" "
-                  required
-                />
-                <label
-                  for="floating_phone"
-                  className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                >
-                  Phone number (123-456-7890)
-                </label>
-              </div>
-              <div className="relative z-0 w-full mb-5 group">
-                <input
-                  type="text"
-                  name="floating_company"
-                  id="floating_company"
-                  className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                  placeholder=" "
-                  required
-                />
-                <label
-                  for="floating_company"
-                  className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                >
-                  Company (Ex. Google)
-                </label>
-              </div>
-            </div>
-            <button
-              type="submit"
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Submit
-            </button>
-          </form>
-        </div>
       </div>
-      {/* <div className="bg-gray-800 text-white p-4  bottom-0 w-full z-50 h-48"></div> */}
     </div>
   );
 };
